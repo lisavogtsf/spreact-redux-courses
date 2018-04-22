047fcf19b652ac22aae8cf12d79f6d2e4f0a1604
Lisa Vogt <lvogt@pivotal.io>
2018-04-21 17:31:07 -0700

    Add header component with routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React, { PropTypes } from "react";
+import Header from '../common/Header';
 
 class App extends React.Component {
   render(){
     return (
       <div className="container-fluid">
-        <p>Header goes here</p>
+        <Header/>
         {this.props.children}
       </div>
     );
-  };
+  }
 }
 
 App.propTypes = {
