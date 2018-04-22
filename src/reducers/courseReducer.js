4011c8ae906f6b550b3a88f77a0e7e433a062e55
Lisa Vogt <lvogt@pivotal.io>
2018-04-22 15:09:52 -0700

    Add first action creator and reducer, plus root reducer, disconnected

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,13 @@
+export default function courseReducer(state = [], action) {
+  switch(action.type) {
+    case 'CREATE_COURSE':
+      // like pushing new action.course onto state array, if it were mutable
+      return [
+        ...state,
+        Object.assign({}, action.course)
+      ];
+
+    default:
+      return state;
+  }
+}
