ee6a4bf9582e25c6925a7b0bf25c92c8a0fcb882
Lisa Vogt <lvogt@pivotal.io>
2018-04-22 15:44:10 -0700

    Shows third way to handle dispatch, using bindActionCreators

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
+import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -21,7 +22,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    this.props.createCourse(this.state.course)
+    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -52,9 +53,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  // dispatch: PropTypes.func.isRequired, // no longer required with mapDispatchToProps
   courses: PropTypes.array.isRequired,
-  createCourse: PropTypes.func.isRequired
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -65,7 +65,7 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    createCourse: course => dispatch(courseActions.createCourse(course))
+    actions: bindActionCreators(courseActions, dispatch)
   };
 }
 
