3950c3e4df796bd44bb51b886406832d3cde47af
Lisa Vogt <lvogt@pivotal.io>
2018-04-22 22:09:56 -0700

    Add ManageCoursePage, Form and Inputs for better editing--not wired up

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,15 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
-import CoursesPage from './components/courses/CoursesPage';
+import CoursesPage from './components/course/CoursesPage';
+import ManageCoursePage from './components/course/ManageCoursePage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="about" component={AboutPage} />
     <Route path="courses" component={CoursesPage} />
+    <Route path="course" component={ManageCoursePage}/>
+    <Route path="course/:id" component={ManageCoursePage} />
   </Route>
 );
