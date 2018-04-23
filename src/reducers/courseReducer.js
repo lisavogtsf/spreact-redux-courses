9bd23f4f4a8ba7af1335507488a800c10553ded7
Lisa Vogt <lvogt@pivotal.io>
2018-04-22 20:01:16 -0700

    Save action types as constants not strings

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import * as types from '../actions/actionTypes';
+
 export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       // like pushing new action.course onto state array, if it were mutable
       return [
         ...state,
