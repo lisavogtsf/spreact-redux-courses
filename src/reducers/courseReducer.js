0a7f9582526ae5cea17ce43ea0838a15b903e50d
Lisa Vogt <lvogt@pivotal.io>
2018-04-22 20:46:56 -0700

    Incorporate thunk and wire up async call to mock api

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,11 +3,12 @@ import * as types from '../actions/actionTypes';
 export default function courseReducer(state = [], action) {
   switch(action.type) {
     case types.CREATE_COURSE:
-      // like pushing new action.course onto state array, if it were mutable
       return [
         ...state,
         Object.assign({}, action.course)
       ];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
 
     default:
       return state;
