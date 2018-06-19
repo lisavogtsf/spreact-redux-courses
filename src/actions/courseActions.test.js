e39f73f66a8e244bd54d93247ff24664bf3908e8
Lisa Vogt <lvogt@pivotal.io>
2018-06-18 17:03:51 -0700

    feedback on ajax calls--track begin and success, add loading dots

--- src/actions/courseActions.test.js
+++ src/actions/courseActions.test.js
@@ -1,6 +1,9 @@
 import expect from 'expect';
 import * as courseActions from './courseActions';
 import * as types from './actionTypes';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
 
 //test a synchronous action
 describe('Course Actions', () => {
@@ -18,6 +21,36 @@ describe('Course Actions', () => {
 
       //assert
       expect(createdCourse).toEqual(expectedAction);
+    });
+  });
+});
+
+const middleware = [ thunk ];
+const mockStore = configureMockStore(middleware);
+
+describe('Course Actions -- async actions', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('should create BEGIN_AJAX_CALL and LOAD_COURSES_SUCCESS when loading courses', (done) => {
+    // sample call to nock with real endpoint vs mockApi
+    // returns desired response
+    // nock('http://www.example.com/')
+    //   .get('/courses')
+    //   .reply(200, { body: { course: [{id: 1, firstName: 'Grace', lastName: 'Hopper'}] }});
+
+    const expectedActions = [
+      { type: types.BEGIN_AJAX_CALL},
+      { type: types.LOAD_COURSES_SUCCESS, body: { course: [{id: 1, firstName: 'Grace', lastName: 'Hopper'}] }}
+    ];
+
+    const store = mockStore({courses: []}, expectedActions);
+    store.dispatch(courseActions.loadCourses()).then(() => {
+      const actions = store.getActions();
+      expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+      expect(actions[1].type).toEqual(types.LOAD_COURSES_SUCCESS);
+      done();
     });
   });
 });
